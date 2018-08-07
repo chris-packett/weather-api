953938c0dca686fd4ce5d5c479bdd6c53e66ccd2
Chris <chrispackett1998@gmail.com>
2018-08-07 16:06:51 -0400

    Implemented geoloc API and weather API

--- public/main.js
+++ public/main.js
@@ -1,5 +1,59 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
+const APP_ID = '&appid=794b4117ce09d9700cccaa55ba8d7d33'
+let LAT
+let LON
+let isCityOrZip = true
+let isLatAndLon = false
+let FETCH_URL
+
+const craftURL = () => {
+  const _baseURL = 'http://api.openweathermap.org/data/2.5/weather?'
+  const _city_or_zip_parameter = 'q='
+  const _lat_parameter = 'lat='
+  const _lon_parameter = '&lon='
+  const _fahrenheit_units = '&units=imperial'
+  let inputValue = document.querySelector('.input').value
+  if (isCityOrZip) {
+    let FETCH_URL = _baseURL + _city_or_zip_parameter + inputValue + _fahrenheit_units + APP_ID
+    return FETCH_URL
+  }
+  else if (isLatAndLon) {
+    let FETCH_URL = _baseURL + _lat_parameter + LAT + _lon_parameter + LON + _fahrenheit_units + APP_ID
+    return FETCH_URL
+  }
 }
 
-document.addEventListener('DOMContentLoaded', main)
+const searchAPI = () => {
+  fetch(craftURL())
+  .then((res) => res.json())
+  .then((data) => {
+    // console.log(data.main.temp)
+    const tempInF = data.main.temp
+    let weatherOutputParent = document.querySelector('.weather-output')
+    let _li = document.createElement('li')
+    _li.textContent = `Temperature in Fahrenheit: ${tempInF}`
+    weatherOutputParent.appendChild(_li)
+  })
+}
+
+const getLocation = () => {
+    isLatAndLon = true
+    isCityOrZip = false
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition)
+    } else { 
+        console.log("Geolocation is not supported by this browser.")
+    }
+}
+
+const showPosition = (position) => {
+  LAT = position.coords.latitude
+  LON = position.coords.longitude
+  console.log(LAT, LON)
+  searchAPI()
+  isLatAndLon = false
+  isCityOrZip = true
+}
+
+document.querySelector('.get-city-or-zip').addEventListener('click', searchAPI)
+document.querySelector('.get-lat-and-lon').addEventListener('click', getLocation)
+
